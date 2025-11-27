6db953fbe1de76158f0c7647de37f7aee9312389
mohamedredachakir <chakirm082@gmail.com>
2025-11-27 14:50:54 +0100

    change strecture css from local to cdn

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>TODO-APP</title>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
